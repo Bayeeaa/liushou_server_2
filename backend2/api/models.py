e2296f0ae84fc056899b35fc83f650ce5be82792
Bayeeaa <2401428618@qq.com>
2025-03-30 13:44:32 +0800

    2025.3.30

--- backend2/api/models.py
+++ backend2/api/models.py
@@ -9,3 +9,13 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+class ProjectOverview(models.Model):
+    title = models.CharField(max_length=200)  # 文章标题
+    content = models.TextField()  # 文章内容
+    created_at = models.DateTimeField(auto_now_add=True)  # 创建时间
+    updated_at = models.DateTimeField(auto_now=True)  # 更新时间
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # 文章作者，关联到 User 表
+
+    def __str__(self):
+        return self.title
